c6218e5def6d76b27e8125c04729526248320662
AnanthGopal <ananth.g1992@gmail.com>
2019-07-15 00:15:38 +0530

    Add files via upload

---
+++ agenda.js
@@ -0,0 +1,184 @@
+export const AgendaDetails = [
+    {
+      id: "1",
+      name: "Guests Arrive",
+      date: "16-Jul-2019",
+      time: "9:30 - 10:00AM",
+      description: "Lorem ipsum...",
+      image: "fa-users"
+    },
+    {
+      id: "2",
+      name: "Ribbon cutting",
+      date: "16-Jul-2019",
+      time: "10:00 AM",
+      description: "Lorem ipsum...",
+      image: "fa-cut"
+    },
+    {
+      id: "3",
+      name: "Diya Lighting & Pooja",
+      date: "16-Jul-2019",
+      time: "10:10 AM",
+      description: "Lorem ipsum...",
+      image: "fa-sun"
+    },
+    {
+      id: "4",
+      name: "Breakfast",
+      date: "16-Jul-2019",
+      time: "10:30 AM - 11:30 AM",
+      description: "Lorem ipsum...",
+      image: "fa-coffee"
+    },
+    {
+      id: "5",
+      name: "Bus Leaves for Resort",
+      date: "16-Jul-2019",
+      time: "11:45 AM",
+      description: "Lorem ipsum...",
+      image: "fa-bus"
+    },
+    {
+      id: "6",
+      name: "Arrive at Resort",
+      date: "16-Jul-2019",
+      time: "12:30 PM",
+      description: "Lorem ipsum...",
+      image: "fa-building"
+    },
+    {
+      id: "7",
+      name: "Lunch",
+      date: "16-Jul-2019",
+      time: "12:30 PM - 14:00 PM",
+      description: "Lorem ipsum...",
+      image: "fa-utensils"
+    },
+    {
+      id: "8",
+      name: "Check-in",
+      date: "16-Jul-2019",
+      time: "14:00 PM - 14:30 PM",
+      description: "Lorem ipsum...",
+      image: "fa-check-circle"
+    },
+    {
+      id: "9",
+      name: "Welcome Note",
+      date: "16-Jul-2019",
+      time: "14:30 PM - 14:35 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-american-sign-language-interpreting"
+    },
+    {
+      id: "10",
+      name: "Panel Discussion",
+      date: "16-Jul-2019",
+      time: "14:35 PM - 15:00 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-columns"
+    },
+    {
+      id: "11",
+      name: "Culturals",
+      date: "16-Jul-2019",
+      time: "15:00 PM - 16:00 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-music"
+    },
+    {
+      id: "12",
+      name: "Presentation/Speech",
+      date: "16-Jul-2019",
+      time: "16:00 PM - 16:30 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-comment-alt"
+    },
+    {
+      id: "13",
+      name: "Vote of Thanks",
+      date: "16-Jul-2019",
+      time: "16:30 PM - 16:40 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-key"
+    },
+    {
+      id: "14",
+      name: "Beach Activities",
+      date: "16-Jul-2019",
+      time: "16:40 PM - 18:30 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-tint"
+    },
+    {
+      id: "15",
+      name: "Break",
+      date: "16-Jul-2019",
+      time: "18:30 PM - 19:00 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-coffee"
+    },
+    {
+      id: "16",
+      name: "Award Ceremony/ Dinner",
+      date: "16-Jul-2019",
+      time: "19:00 PM onwards",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-trophy"
+    },
+    {
+      id: "17",
+      name: "Beach Walk",
+      date: "17-Jul-2019",
+      time: "6:00 AM - 7:00 AM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-tint"
+    },
+    {
+      id: "18",
+      name: "Breakfast",
+      date: "17-Jul-2019",
+      time: "8:00 AM - 10:00 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-coffee"
+    },
+    {
+      id: "19",
+      name: "Check-out",
+      date: "17-Jul-2019",
+      time: "11:30 AM - Noon",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-times-circle"
+    },
+    {
+      id: "15",
+      name: "Bus Leaves for Office",
+      date: "17-Jul-2019",
+      time: "12:00 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-bus"
+    },
+    {
+      id: "15",
+      name: "Bus Arrives at Office",
+      date: "17-Jul-2019",
+      time: "12:45 PM",
+      shortDescription: "",
+      description: "Lorem ipsum...",
+      image: "fa-building"
+    }
+  ];
+  
